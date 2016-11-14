774fcd7c4d27208c57b9f926a1fa69b9b5b9d098
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-11-14 03:46:22 -0400

    [ADD] Styles of the form, yet incomplete.

--- views/forms/viviendo.py
+++ views/forms/viviendo.py
@@ -8,31 +8,54 @@ class FormViviendo(object):
 
     def form(self):
         # Entrada de texto para CI
-        textlabel2=Label(self.root,text="CI:").place(x=100,y=80)
+        Label(self.root,text="CI:").place(x=20,y=83)
         ci=StringVar(self.root, value='0')
-        entry=Entry(self.root,textvariable=ci, width=37).place(x=205,y=80)
+        Entry(self.root,textvariable=ci, width=18, bd=0,
+            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
+            highlightbackground="black",highlightcolor="red",
+            highlightthickness=1).place(x=50,y=80)
 
         # Entrada de texto para Nombre
-        textlabel2=Label(self.root,text="First_name:").place(x=100,y=110)
+        Label(self.root,text="First_name:").place(x=20,y=110)
         first_name=StringVar()
-        entry=Entry(self.root,textvariable=first_name, width=37).place(x=205,y=110)
+        Entry(self.root,textvariable=first_name, width=37, bd=0,
+            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
+            disabledbackground="#1E6FBA",disabledforeground="yellow",
+            highlightbackground="black",highlightcolor="red",
+            highlightthickness=1).place(x=205,y=110)
 
         # Entrada de texto para Aprellido
-        textlabel2=Label(self.root,text="Last_name:").place(x=100,y=140)
+        Label(self.root,text="Last_name:").place(x=20,y=140)
         last_name=StringVar()
-        entry=Entry(self.root,textvariable=last_name, width=37).place(x=205,y=140)
+        Entry(self.root,textvariable=last_name, width=37, bd=0,
+            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
+            disabledbackground="#1E6FBA",disabledforeground="yellow",
+            highlightbackground="black",highlightcolor="red",
+            highlightthickness=1).place(x=205,y=140)
 
         # Entrada de texto para direction
-        textlabel2=Label(self.root,text="Direccion:").place(x=100,y=170)
+        Label(self.root,text="Direccion:").place(x=20,y=170)
         direction=StringVar()
-        entry=Entry(self.root,textvariable=direction, width=37).place(x=205,y=170)
+        Entry(self.root,textvariable=direction, width=37, bd=0,
+            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
+            disabledbackground="#1E6FBA",disabledforeground="yellow",
+            highlightbackground="black",highlightcolor="red",
+            highlightthickness=1).place(x=205,y=170)
 
         # Entrada de texto para Fecha de Nacimiento
-        textlabel2=Label(self.root,text="Fecha de Nacimiento:").place(x=100,y=200)
+        Label(self.root,text="Fecha de Nacimiento:").place(x=20,y=200)
         birthday=StringVar()
-        entry=Entry(self.root,textvariable=birthday, width=37).place(x=205,y=200)
+        Entry(self.root,textvariable=birthday, width=37, bd=0,
+            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
+            disabledbackground="#1E6FBA",disabledforeground="yellow",
+            highlightbackground="black",highlightcolor="red",
+            highlightthickness=1).place(x=205,y=200)
 
         # Entrada de texto para estado_civil
-        textlabel2=Label(self.root,text="Estado Civil:").place(x=100,y=230)
+        Label(self.root,text="Estado Civil:").place(x=20,y=230)
         estado_civil=StringVar()
-        entry=Entry(self.root,textvariable=estado_civil, width=37).place(x=205,y=230)
+        Entry(self.root,textvariable=estado_civil, width=37, bd=0,
+            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
+            disabledbackground="#1E6FBA",disabledforeground="yellow",
+            highlightbackground="black",highlightcolor="red",
+            highlightthickness=1).place(x=205,y=230)
