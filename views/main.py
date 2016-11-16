1f48261e1bddd9ff29f3b6efd5b24edcb6d340ce
unknown <moises-jesus_15@hotmail.com>
2016-11-16 13:06:55 -0430

    [UPDATE] resolution modification.

--- views/main.py
+++ views/main.py
@@ -9,7 +9,7 @@ class Main(object):
     def __init__(self, root):
         self.root = root
         self.root.title('Home')
-        self.root.geometry("800x700+300+300")
+        self.root.geometry("830x700+300+300")
         tool = Toolbar(self.root)
 
 if __name__ == "__main__":
