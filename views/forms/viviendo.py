a2ab32e349a8dd887cf0eb086aed9e4b7cf9af76
unknown <moises-jesus_15@hotmail.com>
2016-11-17 06:49:01 -0430

    [UPDATE] Imports Modifieds from file form/viviendo.

--- views/forms/viviendo.py
+++ views/forms/viviendo.py
@@ -1,124 +1,134 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Tkinter import *
-# import ttkcalendar
+import Tkinter as tk
+from views.utils._calendar import CalendarDialog
 
-class ViviendoForm(Frame):
+class ViviendoForm(tk.Frame):
 
     def __init__(self, root):
-        Frame.__init__(self, root)
+        tk.Frame.__init__(self, root)
         self.root = root
         self.form()
 
+    def getDate(self):
+        cd = CalendarDialog(self)
+        result = cd.result
+        try:
+            self.birthday.set(result.strftime("%d/%m/%Y"))
+        except AttributeError, e:
+            self.birthday.set(self.birthday.get())
+
     def form(self):
         # Title of the Form
-        Label(self.root,text="Registro de Viviendo", font="Helvetica 16 bold",
+        tk.Label(self.root,text="Registro de Viviendo", font="Helvetica 16 bold",
             fg="blue").place(x=310,y=60)
 
         # Entry of the cedula
-        Label(self.root,text="CI:", font="Helvetica 12",
+        tk.Label(self.root,text="CI:", font="Helvetica 12",
             fg="#474747").place(x=70,y=130)
-        ci=StringVar(self.root, value='0')
-        Entry(self.root,textvariable=ci, width=20, bd=0,
+        ci=tk.StringVar(self.root, value='0')
+        tk.Entry(self.root,textvariable=ci, width=20, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=100,y=130)
 
         # Entrada de texto para Nombre
-        Label(self.root,text="Nombre:", font="Helvetica 12",
+        tk.Label(self.root,text="Nombre:", font="Helvetica 12",
             fg="#474747").place(x=27,y=165)
-        first_name=StringVar()
-        Entry(self.root,textvariable=first_name, width=20, bd=0,
+        first_name=tk.StringVar()
+        tk.Entry(self.root,textvariable=first_name, width=20, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=100,y=165)
 
         # Entrada de texto para Apellido
-        Label(self.root,text="Apellido:", font="Helvetica 12",
+        tk.Label(self.root,text="Apellido:", font="Helvetica 12",
             fg="#474747").place(x=26,y=200)
-        last_name=StringVar()
-        Entry(self.root,textvariable=last_name, width=20, bd=0,
+        last_name=tk.StringVar()
+        tk.Entry(self.root,textvariable=last_name, width=20, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=100,y=200)
 
         # Entrada de texto para direction
-        Label(self.root,text="Fecha de Nacimiento:", font="Helvetica 12",
-            fg="#474747").place(x=20,y=235)
-        birthday=StringVar()
-        # ttkcalendar.Calendar(self.root).pack()
-        Entry(self.root,textvariable=birthday, width=12, bd=0,
+        tk.Label(self.root,text="Fecha de Nacimiento:", font="Helvetica 12",
+            fg="#474747").place(x=10,y=235)
+        self.birthday=tk.StringVar()
+        # select date
+        tk.Button(self.root, text="Cal", bg="#1E6FBA", fg="yellow",
+            command=self.getDate).place(x=295,y=235)
+        tk.Entry(self.root,textvariable=self.birthday, width=10, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
-            highlightthickness=1).place(x=181,y=235)
+            highlightthickness=1).place(x=170,y=235)
 
 
         # Fields right
         # Entrada de texto para Fecha de Nacimiento
-        Label(self.root,text="Dirección:", font="Helvetica 12",
+        tk.Label(self.root,text="Dirección:", font="Helvetica 12",
             fg="#474747").place(x=420,y=130)
-        direction=StringVar()
-        Entry(self.root,textvariable=direction, width=27, bd=0,
+        direction=tk.StringVar()
+        tk.Entry(self.root,textvariable=direction, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=500,y=130)
 
         # Entrada de texto para estado_civil
-        Label(self.root,text="Estado Civil:", font="Helvetica 12",
+        tk.Label(self.root,text="Estado Civil:", font="Helvetica 12",
             fg="#474747").place(x=400,y=165)
-        estado_civil=StringVar()
-        Entry(self.root,textvariable=estado_civil, width=27, bd=0,
+        estado_civil=tk.StringVar()
+        tk.Entry(self.root,textvariable=estado_civil, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=500,y=165)
 
         # Entrada de texto para work BOOLEAN
-        Label(self.root,text="Trabaja:", font="Helvetica 12",
+        tk.Label(self.root,text="Trabaja:", font="Helvetica 12",
             fg="#474747").place(x=430,y=200)
-        work=BooleanVar(self.root, value=False)
-        Checkbutton(self.root,textvariable=work, font="Helvetica 14 normal",
+        work=tk.BooleanVar(self.root, value=False)
+        tk.Checkbutton(self.root,textvariable=work, font="Helvetica 14 normal",
             bd=0, bg="#1E6FBA", fg="black", highlightbackground="black",
             highlightcolor="red").place(x=500,y=200)
 
         # Entrada de texto para Ingresos
-        Label(self.root,text="Ingresos:", font="Helvetica 12",
+        tk.Label(self.root,text="Ingresos:", font="Helvetica 12",
             fg="#474747").place(x=425,y=235)
-        estado_civil=StringVar()
-        Entry(self.root,textvariable=estado_civil, width=27, bd=0,
+        estado_civil=tk.StringVar()
+        tk.Entry(self.root,textvariable=estado_civil, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=500,y=235)
 
         # Entrada de texto para postulation
-        Label(self.root,text="Postulación:", font="Helvetica 12",
+        tk.Label(self.root,text="Postulación:", font="Helvetica 12",
             fg="#474747").place(x=401,y=270)
-        postulation=StringVar()
-        Entry(self.root,textvariable=postulation, width=27, bd=0,
+        postulation=tk.StringVar()
+        tk.Entry(self.root,textvariable=postulation, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=500,y=270)
 
         # Entrada de texto para discapacity BOOLEAN
-        Label(self.root,text="Discapacidad:", font="Helvetica 12",
+        tk.Label(self.root,text="Discapacidad:", font="Helvetica 12",
             fg="#474747").place(x=390,y=305)
-        discapacity=BooleanVar(self.root, value=False)
-        Checkbutton(self.root,textvariable=discapacity, font="Helvetica 14 normal",
+        discapacity=tk.BooleanVar(self.root, value=False)
+        tk.Checkbutton(self.root,textvariable=discapacity, font="Helvetica 14 normal",
             bd=0, bg="#1E6FBA", fg="black", highlightbackground="black",
             highlightcolor="red").place(x=500,y=305)
 
         # Entrada de texto para discapacity Description
-        Label(self.root,text="Describa Discapacidad:", font="Helvetica 12",
+        tk.Label(self.root,text="Describa Discapacidad:", font="Helvetica 12",
             fg="#474747").place(x=320,y=340)
-        discapacity_desc=StringVar()
-        Text(self.root, width=30, height=4, bd=0,
+        discapacity_desc=tk.StringVar()
+        tk.Text(self.root, width=30, height=4, bd=0,
             font="Helvetica 12 normal",bg="#1E6FBA",fg="yellow",
             highlightbackground="black",highlightcolor="red",
             highlightthickness=1).place(x=500,y=340)
