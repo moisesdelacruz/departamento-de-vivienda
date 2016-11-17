719355558c429fe2e329cad2b302b1a859be64bd
Moises De La Cruz <moises-jesus_15@hotmail.com>
2016-11-17 09:55:10 -0430

    [ADD] Adding validations in form Viviendo. yet incomplete.

--- views/forms/viviendo.py
+++ views/forms/viviendo.py
@@ -11,6 +11,8 @@ class ViviendoForm(tk.Frame):
     def __init__(self, root):
         tk.Frame.__init__(self, root)
         self.root = root
+        self.validate_number = (self.root.register(self.validate),
+                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
         self.form()
 
     def getDate(self):
@@ -26,6 +28,33 @@ class ViviendoForm(tk.Frame):
         self.img = self.img.resize((sizeY, sizeX), Image.ANTIALIAS)
         return ImageTk.PhotoImage(self.img)
 
+    def validate(self, action, index, value_if_allowed,
+                       prior_value, text, validation_type, trigger_type, widget_name):
+        if text in '0123456789.-+':
+            try:
+                float(value_if_allowed)
+                return True
+            except ValueError:
+                return False
+        else:
+            return False
+
+    def save(self):
+        data = ({
+            "ci": int(self.ci.get()),
+            "first_name": self.first_name.get(),
+            "last_name": self.last_name.get(),
+            "direction": self.direction.get(),
+            "birthday": self.birthday.get(),
+            "estado_civil": self.estado_civil.get(),
+            "work": self.work.get(),
+            "entry": float(self.entry.get()),
+            "postulation": self.postulation.get(),
+            "discapacity": self.discapacity.get(),
+            "discapacity_desc": str(self.discapacity_desc.get('0.0',tk.END))
+        })
+        print data
+
     def form(self):
         # Title of the Form
         tk.Label(self.root,text="Registro de Viviendo", font="Helvetica 16 bold",
@@ -34,17 +63,18 @@ class ViviendoForm(tk.Frame):
         # Entry of the cedula
         tk.Label(self.root,text="CI:", font="Helvetica 12",
             fg="#474747").place(x=70,y=130)
-        ci=tk.StringVar(self.root, value='0')
-        tk.Entry(self.root,textvariable=ci, width=20, bd=0,
-            font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
-            highlightbackground="black",highlightcolor="red",
-            highlightthickness=1).place(x=100,y=130)
+        self.ci=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.ci, validate='key',
+            validatecommand=self.validate_number,
+            width=20, bd=0, font="Helvetica 14 normal",justify="left",
+            bg="#1E6FBA",fg="yellow", highlightbackground="black",
+            highlightcolor="red", highlightthickness=1).place(x=100,y=130)
 
         # Entrada de texto para Nombre
         tk.Label(self.root,text="Nombre:", font="Helvetica 12",
             fg="#474747").place(x=27,y=165)
-        first_name=tk.StringVar()
-        tk.Entry(self.root,textvariable=first_name, width=20, bd=0,
+        self.first_name=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.first_name, width=20, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
@@ -53,8 +83,8 @@ class ViviendoForm(tk.Frame):
         # Entrada de texto para Apellido
         tk.Label(self.root,text="Apellido:", font="Helvetica 12",
             fg="#474747").place(x=26,y=200)
-        last_name=tk.StringVar()
-        tk.Entry(self.root,textvariable=last_name, width=20, bd=0,
+        self.last_name=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.last_name, width=20, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
@@ -83,8 +113,8 @@ class ViviendoForm(tk.Frame):
         # Entrada de texto para Fecha de Nacimiento
         tk.Label(self.root,text="Dirección:", font="Helvetica 12",
             fg="#474747").place(x=420,y=130)
-        direction=tk.StringVar()
-        tk.Entry(self.root,textvariable=direction, width=27, bd=0,
+        self.direction=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.direction, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
@@ -93,8 +123,8 @@ class ViviendoForm(tk.Frame):
         # Entrada de texto para estado_civil
         tk.Label(self.root,text="Estado Civil:", font="Helvetica 12",
             fg="#474747").place(x=400,y=165)
-        estado_civil=tk.StringVar()
-        tk.Entry(self.root,textvariable=estado_civil, width=27, bd=0,
+        self.estado_civil=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.estado_civil, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
@@ -103,16 +133,16 @@ class ViviendoForm(tk.Frame):
         # Entrada de texto para work BOOLEAN
         tk.Label(self.root,text="Trabaja:", font="Helvetica 12",
             fg="#474747").place(x=430,y=200)
-        work=tk.BooleanVar(self.root, value=False)
-        tk.Checkbutton(self.root,textvariable=work, font="Helvetica 14 normal",
+        self.work=tk.BooleanVar(self.root, value=False)
+        tk.Checkbutton(self.root,textvariable=self.work, font="Helvetica 14 normal",
             bd=0, bg="#1E6FBA", fg="black", highlightbackground="black",
             highlightcolor="red").place(x=500,y=200)
 
         # Entrada de texto para Ingresos
         tk.Label(self.root,text="Ingresos:", font="Helvetica 12",
             fg="#474747").place(x=425,y=235)
-        estado_civil=tk.StringVar()
-        tk.Entry(self.root,textvariable=estado_civil, width=27, bd=0,
+        self.entry=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.entry, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
@@ -121,8 +151,8 @@ class ViviendoForm(tk.Frame):
         # Entrada de texto para postulation
         tk.Label(self.root,text="Postulación:", font="Helvetica 12",
             fg="#474747").place(x=401,y=270)
-        postulation=tk.StringVar()
-        tk.Entry(self.root,textvariable=postulation, width=27, bd=0,
+        self.postulation=tk.StringVar()
+        tk.Entry(self.root,textvariable=self.postulation, width=27, bd=0,
             font="Helvetica 14 normal",justify="left",bg="#1E6FBA",fg="yellow",
             disabledbackground="#1E6FBA",disabledforeground="yellow",
             highlightbackground="black",highlightcolor="red",
@@ -131,16 +161,22 @@ class ViviendoForm(tk.Frame):
         # Entrada de texto para discapacity BOOLEAN
         tk.Label(self.root,text="Discapacidad:", font="Helvetica 12",
             fg="#474747").place(x=390,y=305)
-        discapacity=tk.BooleanVar(self.root, value=False)
-        tk.Checkbutton(self.root,textvariable=discapacity, font="Helvetica 14 normal",
+        self.discapacity=tk.BooleanVar(self.root, value=False)
+        tk.Checkbutton(self.root,textvariable=self.discapacity, font="Helvetica 14 normal",
             bd=0, bg="#1E6FBA", fg="black", highlightbackground="black",
             highlightcolor="red").place(x=500,y=305)
 
         # Entrada de texto para discapacity Description
         tk.Label(self.root,text="Describa Discapacidad:", font="Helvetica 12",
             fg="#474747").place(x=320,y=340)
-        discapacity_desc=tk.StringVar()
-        tk.Text(self.root, width=30, height=4, bd=0,
+        self.discapacity_desc=tk.Text(self.root, width=30, height=4, bd=0,
             font="Helvetica 12 normal",bg="#1E6FBA",fg="yellow",
             highlightbackground="black",highlightcolor="red",
-            highlightthickness=1).place(x=500,y=340)
+            highlightthickness=1)
+        self.discapacity_desc.place(x=500,y=340)
+
+        # Buttons of actions
+        # Guardar
+        tk.Button(self.root, command=self.save, text="Guardar", font="Helvetica 14 normal", bd=0,
+            activebackground="red", activeforeground="blue",
+            bg="green", fg="white", width=12, height=2).place(x=500,y=500)
