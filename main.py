763781d6cb34aa051a7a0014b69d519fcaf7b505
Moises De La Cruz <moises-jesus_15@hotmail.com>
2016-11-23 10:28:10 -0430

    [UPDATE] new resolution.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ class Main(object):
     def __init__(self, root):
         self.root = root
         self.root.title('Home')
-        self.root.minsize(820,500)
-        self.root.maxsize(850,800)
-        self.root.geometry("830x700+300+300")
+        self.root.minsize(920,540)
+        # self.root.maxsize(850,800)
+        self.root.geometry("1000x720+300+300")
         tool = Toolbar(self.root)
 
 if __name__ == "__main__":
