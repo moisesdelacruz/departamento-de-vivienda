18f214d7ca7b150b7b56574c954a715a9b8fc1db
Moises De La Cruz <moises-jesus_15@hotmail.com>
2016-11-24 09:57:40 -0430

    [ADD] Validations of field from window emergent

---
+++ utils/askfloat.py
@@ -0,0 +1,20 @@
+import Tkinter as tk
+from lib import tkSimpleDialog
+import validate
+
+
+class FloatDialog(tkSimpleDialog.Dialog):
+	"""Dialog box that displays a text fields and return it"""
+	def body(self, master, message):
+		tk.Label(master, text=message,
+			font="Helvetica 12 normal").pack(side=tk.TOP, pady=10)
+
+		self.value=validate.FloatEntry(master,
+			width=12, bd=0, font="Helvetica 14 normal",
+			justify="left",bg="#1E6FBA",fg="yellow",
+			highlightbackground="black",highlightcolor="red",
+			highlightthickness=1)
+		self.value.pack(pady=8)
+
+	def apply(self):
+		self.result = float(self.value.get())
