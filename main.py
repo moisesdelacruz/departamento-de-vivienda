7f35e74b1c48f273917b87806b8e8980f35fb734
Moises De La Cruz <moises-jesus_15@hotmail.com>
2016-12-06 20:25:33 -0430

    [UPDATE]  min size of window adjusted

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Main(object):
     def __init__(self, root):
         self.root = root
         self.root.title('Home')
-        self.root.minsize(920,540)
+        self.root.minsize(950,620)
         # self.root.maxsize(850,800)
         self.root.geometry("1000x720+300+300")
         tool = Toolbar(self.root)
