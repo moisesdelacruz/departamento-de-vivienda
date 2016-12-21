e494de4a12b61719f5f5be70d57b8ed85851051f
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-12-20 23:48:55 -0400

    [ADD] create class dynamically con 'type' in database module.

--- database/main.py
+++ database/main.py
@@ -5,25 +5,12 @@ from family import Family
 from user import User
 from tracing import Tracing
 
-connection = psycopg2.connect(database='viviendo_db',user='postgres',
-    password='demilovato', host='localhost')
+connection = psycopg2.connect(database='viviendo_db',user='moises',
+    password='christinagrimmie', host='localhost')
 
-class ViviendoModel(Viviendo):
+models = ['Viviendo', 'Solicitud', 'Family', 'User', 'Tracing']
 
+for model in models:
     conn = connection
-
-class SolicitudModel(Solicitud):
-
-    conn = connection
-
-class FamilyModel(Family):
-
-    conn = connection
-
-class UserModel(User):
-
-	conn = connection
-
-class TracingModel(Tracing):
-
-	conn = connection
+    locals()[model+'Model'] = type('"'+model+'Model'+'"',
+        (eval(model),), {'conn': conn})
