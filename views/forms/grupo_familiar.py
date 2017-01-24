4a89e69c5ddcfd26f6e88f83cdaf8659ffa64c17
moisesdelacruz <moises-jesus_15@hotmail.com>
2017-01-24 18:27:36 -0400

    [ADD] family form view add fields required and message of error.

--- views/forms/grupo_familiar.py
+++ views/forms/grupo_familiar.py
@@ -20,6 +20,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		self.viviendo_id = viviendo_id
 		# instance database
 		self.db = FamilyModel()
+		# data actually
+		self.data = ({})
 		# edit mode unavailable
 		self.edit = False
 		# state actual of the form
@@ -29,7 +31,7 @@ class Grupo_familiarForm(tk.Frame, Methods):
 			self.action = kwargs.get('action')
 			self._actions(kwargs)
 
-		self.render()
+		self.show()
 
 
 	def _actions(self, kwargs):
@@ -39,6 +41,7 @@ class Grupo_familiarForm(tk.Frame, Methods):
 			# title of window
 			self.controller.parent.title('Editar Familiar')
 			self.data = kwargs.get('data')
+			self.data_id = self.data.get('id')
 			# edit mode available
 			self.edit = True
 
@@ -49,25 +52,7 @@ class Grupo_familiarForm(tk.Frame, Methods):
 
 
 	def save(self):
-		data = ({
-			"viviendo_id": int(self.viviendo_id),
-			"nationality": self.nationality.get(),
-			"ci": int(self.ci.get()),
-			"first_name": self.first_name.get(),
-			"last_name": self.last_name.get(),
-			"birthday": self.birthday.get(),
-			"sex": self.sex.get(),
-			"estado_civil": self.estado_civil.get(),
-			"instructional_level": self.instructional_level.get(),
-			"work": bool(self.work.get()),
-			"occupation": self.occupation.get(),
-			"institution": self.institution.get(),
-			"entry": float(self.entry),
-			"birth_state": self.birth_state.get(),
-			"discapacity": bool(self.discapacity.get()),
-			"discapacity_desc": str(self.discapacity_desc),
-			"old_age": bool(self.old_age.get())
-		})
+		data = self._format()
 
 		if not self.edit:
 			self.db.create(data)
@@ -77,7 +62,7 @@ class Grupo_familiarForm(tk.Frame, Methods):
 			view = detail.grupo_familiar.Grupo_familiarDetail(self.root,
 				self.controller, self.viviendo_id)
 		elif self.edit:
-			data['id'] = self.data.get('id')
+			data['id'] = self.data_id
 			self.db.update(data)
 			# clean
 			self.clean(self.root)
@@ -85,60 +70,106 @@ class Grupo_familiarForm(tk.Frame, Methods):
 			view = detail.grupo_familiar.Grupo_familiarDetail(self.root,
 				self.controller, self.viviendo_id)
 
+	def _format(self):
+		fields = [{'int': 'viviendo_id'}, {'str': 'nationality'}, {'int': 'ci'},
+			{'str': 'first_name'}, {'str': 'last_name'}, {'str': 'birthday'},
+			{'str': 'sex'}, {'str': 'estado_civil'}, {'str': 'instructional_level'},
+			{'bool': 'work'}, {'str': 'occupation'}, {'str': 'institution'},
+			{'float': 'entry'}, {'str': 'birth_state'}, {'bool': 'discapacity'},
+			{'str': 'discapacity_desc'}, {'bool': 'old_age'}]
+
+		data = ({})
+		for field in fields:
+			for key, value in field.items():
+				if (value != 'viviendo_id' and value != 'entry'
+					and value != 'discapacity_desc'):
+					data[value] = eval(
+						key+'(self.'+value+'.get())') if hasattr(
+						self, value) else self.data.get(value)
+				else:
+					data[value] = eval(
+						key+'(self.'+value+')') if hasattr(
+						self, value) else self.data.get(value)
+		return data
+
+	def state_data(self):
+		self.data = self._format()
+
+	def validate(self):
+		required_list = ['nationality', 'ci', 'first_name', 'last_name', 'sex']
+		for item in required_list:
+			if hasattr(self, item):
+				if not bool(eval('self.%s.get()' %(item))):
+					self.set_error('%s es requerido para continuar' %(item))
+					return False
+					break
+				if item == 'ci' and len(self.ci.get()) < 7:
+					self.set_error('%s debe tener al menos ocho digitos para continuar' %(item))
+					return False
+					break
+			else: pass
+		return True
+
 	def change_form(self, action):
-		self.clean(self._form)
-		if action == 'next':
+		if action == 'next' and self.validate():
 			self.form_actually += 1
-		elif action == 'back':
+			self.state_data()
+			self.render()
+		elif action == 'back' and self.validate():
 			self.form_actually -= 1
+			self.state_data()
+			self.render()
 		elif action == 'render':
-			pass
-
-		if hasattr(self, eval("'form%s'" %(str(self.form_actually)))):
-			# Title of the Form
-			ttk.Label(self._form,
-				text="Editar Familiar" if self.edit else "Registrar Familiar",
-				style='Black22.TLabel').pack(anchor=tk.NW)
-
-			ttk.Label(self._form, text="Sera redirigido automaticamente"+
-				"a un panel de administracion personalizado,"+
-				"\nAl Presionar \"Guardar\".",
-				style="Black12.TLabel").pack(anchor=tk.NW, pady=15)
-			# render form
-			eval('self.form%s()' %(str(self.form_actually)))
-			# content message
-			self.content_message = ttk.Frame(self._form,
-				style='Kim.TFrame')
-			self.content_message.pack(pady=8)
-
+			self.render()
 
 	def set_error(self, err):
 		self.clean(self.content_message)
 		ttk.Label(self.content_message,
 			text=err, style='Error.TLabel').pack()
 
-
 	def render(self):
+		# clean content form
+		self.clean(self.content_form)
+
+		if hasattr(self, eval("'form%s'" %(str(self.form_actually)))):
+			# Title of the Form
+			ttk.Label(self.content_form,
+				text="Editar Familiar" if self.edit else "Registrar Familiar",
+				style='Black22.TLabel').pack(anchor=tk.NW)
+
+			ttk.Label(self.content_form, text="Sera redirigido automaticamente"+
+				"a un panel de administracion personalizado,"+
+				"\nAl Presionar \"Guardar\".",
+				style="Black12.TLabel").pack(anchor=tk.NW, pady=15)
+			# render form
+			eval('self.form%s()' %(str(self.form_actually)))
+			# content message
+			self.content_message = ttk.Frame(self.content_form,
+				style='Kim.TFrame')
+			self.content_message.pack(pady=8)
+
+	def show(self):
 		# boxs------
 		div = ttk.Frame(self.root, height=550, style='Kim.TFrame')
 		div.pack(expand=True, fill=tk.X)
 		div.pack_propagate(0)
 
-		self._form = ttk.Frame(div, width=650, padding=20, style='White.TFrame')
-		self._form.pack(expand=True, fill=tk.Y)
-		self._form.pack_propagate(0)
+		self.content_form = ttk.Frame(div, width=650, padding=20, style='White.TFrame')
+		self.content_form.pack(expand=True, fill=tk.Y)
+		self.content_form.pack_propagate(0)
 
 		# call form1
 		self.change_form('render')
 
 	def form1(self):
-		form=self._form
+		form=self.content_form
 
 		# Nacionalidad
 		ttk.Label(form, text='Nacionalidad',
 			style='Black.TLabel').place(x=0, y=130)
 		self.nationality = tk.StringVar(form,
-			value=self.data.get('nationality') if self.edit else '')
+			value=self.data.get('nationality')
+				if self.data.get('nationality') else '')
 		fieldNationality = ttk.Combobox(form, state='readonly',
 			textvariable=self.nationality, font="Helvetica 13", justify="left",
 			background="#1E6FBA", width=25)
@@ -146,23 +177,24 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		for (x, item) in enumerate(fieldNationality['values']):
 			if item == self.nationality.get():
 				fieldNationality.current(int(x))
+		fieldNationality.focus()
 		fieldNationality.pack(pady=8)
 
 		# Entry of the cedula
 		ttk.Label(form,text="CI",
 			style='Black.TLabel').place(x=0,y=180)
 		self.ci=validate.IntegerEntry(form, value=self.data.get('ci')
-				if self.edit else 0, maxlength=9,
+				if self.data.get('ci') else 0, maxlength=9,
 			style='White.TEntry', width=27,
 			font="Helvetica 13", justify="left")
-		self.ci.focus()
 		self.ci.pack(pady=8)
 
 		# Entry of first_name
 		ttk.Label(form,text="Nombre",
 			style='Black.TLabel').place(x=0,y=230)
 		self.first_name=validate.MaxLengthEntry(form,
-			value=self.data.get('first_name') if self.edit else '',
+			value=self.data.get('first_name')
+				if self.data.get('first_name') else '',
 			style='White.TEntry', maxlength=30,width=27,
 			font="Helvetica 13",justify="left")
 		self.first_name.pack(pady=8)
@@ -171,7 +203,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form,text="Apellido",
 			style='Black.TLabel').place(x=0,y=275)
 		self.last_name=validate.MaxLengthEntry(form,
-			value=self.data.get('last_name') if self.edit else '',
+			value=self.data.get('last_name')
+				if self.data.get('last_name') else '',
 			style='White.TEntry', maxlength=30, width=27,
 			font="Helvetica 13", justify="left")
 		self.last_name.pack(pady=8)
@@ -180,7 +213,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form, text="Fecha de Nacimiento",
 			style='Black.TLabel').place(x=0,y=330)
 
-		actually = self.data.get('birthday') if self.edit else None
+		actually = (self.data.get('birthday')
+			if self.data.get('birthday') else None)
 
 		self.birthday=entrydate.DateEntry(form, actually=actually,
 			style='White.TEntry', font="Helvetica 13")
@@ -190,7 +224,7 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form, text="Sexo:",
 			style='Black.TLabel').place(x=0,y=380)
 		self.sex = tk.StringVar(form,
-			value=self.data.get('sex') if self.edit else '')
+			value=self.data.get('sex') if self.data.get('sex') else '')
 		fieldSex = ttk.Combobox(form, state='readonly', textvariable=self.sex,
 			font="Helvetica 13", justify="left",background="#1E6FBA", width=25)
 		fieldSex['values'] = self.select_sex()
@@ -211,13 +245,14 @@ class Grupo_familiarForm(tk.Frame, Methods):
 			text="Siguiente").pack(side=tk.LEFT, padx=8)
 
 	def form2(self):
-		form = self._form
+		form = self.content_form
 
 		# Entrada de texto para estado_civil
 		ttk.Label(form, text="Estado Civil:",
 			style='Black.TLabel').place(x=0,y=135)
 		self.estado_civil=tk.StringVar(form,
-			value=self.data.get('estado_civil') if self.edit else '')
+			value=self.data.get('estado_civil')
+				if self.data.get('estado_civil') else '')
 		fieldState_civil = ttk.Combobox(form, state='readonly',
 			textvariable=self.estado_civil, font="Helvetica 13",
 			justify="left",background="#1E6FBA", width=25)
@@ -231,7 +266,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form, text="Niv. Instruccional:",
 			style='Black.TLabel').place(x=0,y=185)
 		self.instructional_level = tk.StringVar(form,
-			value=self.data.get('instructional_level') if self.edit else '')
+			value=self.data.get('instructional_level')
+				if self.data.get('instructional_level') else '')
 		fieldNiv_Inst = ttk.Combobox(form, state='readonly',
 		textvariable=self.instructional_level,
 			font="Helvetica 13", justify="left",background="#1E6FBA", width=25)
@@ -245,7 +281,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form, text="Occupacion:",
 			style='Black.TLabel').place(x=0,y=230)
 		self.occupation=validate.MaxLengthEntry(form, style='White.TEntry',
-			value=self.data.get('occupation') if self.edit else '',
+			value=self.data.get('occupation')
+				if self.data.get('occupation') else '',
 			maxlength=40, width=27, font="Helvetica 13",justify="left")
 		self.occupation.pack(pady=8)
 
@@ -253,7 +290,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form, text="Institucion:",
 			style='Black.TLabel').place(x=0,y=280)
 		self.institution=validate.MaxLengthEntry(form, style='White.TEntry',
-			value=self.data.get('institution') if self.edit else '',
+			value=self.data.get('institution')
+				if self.data.get('institution') else '',
 			maxlength=40, width=27, font="Helvetica 13",justify="left")
 		self.institution.pack(pady=8)
 
@@ -261,7 +299,8 @@ class Grupo_familiarForm(tk.Frame, Methods):
 		ttk.Label(form,text="Estado de Nacimiento",
 			style='Black.TLabel').place(x=0,y=335)
 		self.birth_state=validate.MaxLengthEntry(form,
-			value=self.data.get('birth_state') if self.edit else '',
+			value=self.data.get('birth_state')
+				if self.data.get('birth_state') else '',
 			style='White.TEntry', maxlength=30, width=27,
 			font="Helvetica 13", justify="left")
 		self.birth_state.pack(pady=8)
@@ -273,23 +312,25 @@ class Grupo_familiarForm(tk.Frame, Methods):
 
 		# Entry of tercera edad
 		self.old_age=tk.BooleanVar(booleans,
-			value=self.data.get('old_age') if self.edit else False)
+			value=self.data.get('old_age') if self.data.get('old_age') else False)
 		ttk.Checkbutton(booleans, text='Tercera edad', variable=self.old_age,
 			onvalue=True, offvalue=False).pack(side=tk.LEFT, padx=5, pady=8)
 
 		# Entry of discapacity
-		self.discapacity_desc=self.data.get('discapacity_desc') if self.edit else ''
+		self.discapacity_desc=(self.data.get('discapacity_desc')
+			if self.data.get('discapacity_desc') else '')
 		self.discapacity=tk.BooleanVar(booleans,
-			value=self.data.get('discapacity') if self.edit else False)
+			value=self.data.get('discapacity')
+				if self.data.get('discapacity') else False)
 		ttk.Checkbutton(booleans, text='Discapacidad', variable=self.discapacity,
 			onvalue=True, offvalue=False,
 			command=lambda : self.textDialog(self.discapacity_desc)
 			).pack(side=tk.LEFT, padx=5, pady=8)
 
 		# Boolean of work
-		self.entry=self.data.get('entry') if self.edit else 0
+		self.entry=self.data.get('entry') if self.data.get('entry') else 0
 		self.work=tk.BooleanVar(booleans,
-			value=self.data.get('work') if self.edit else False)
+			value=self.data.get('work') if self.data.get('work') else False)
 		ttk.Checkbutton(booleans, text='Trabaja', variable=self.work,
 			onvalue=True, offvalue=False,
 			command=lambda : self.floatDialog(self.entry)
